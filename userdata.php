ad065afaa307ce459a108e23a1b07f5fc3e3dd4f
Ehad Jarral <jarraleddie@gmail.com>
2019-11-14 16:50:16 -0600

    Unauthorized access redirects to login page

--- userdata.php
+++ userdata.php
@@ -1,12 +1,18 @@
 <?php
     session_start();
+require_once "controller/template.controller.php";
 require_once "controller/users.controller.php";
+require_once "controller/roles.controller.php";
 
 require_once "model/users.model.php";
+require_once "model/roles.model.php";
     ?>
 
 <?php
 if (isset($_SESSION["beginSession"])&&$_SESSION["beginSession"] == "ok" && $_SESSION["roles_id"]=="2"){
     include "view/modules/users.php";
+}
+else{
+    include "view/modules/login.php";
 }
 ?>
